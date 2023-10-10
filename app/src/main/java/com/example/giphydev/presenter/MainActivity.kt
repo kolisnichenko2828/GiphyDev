61f0cc2e4912b274c5634c96ceb8c18b11dc269b
kolisnichenko2828 <kolisnichenko2828@gmail.com>
2023-10-10 23:35:21 +0300

    added listener for new line and space

--- app/src/main/java/com/example/giphydev/presenter/MainActivity.kt
+++ app/src/main/java/com/example/giphydev/presenter/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.giphydev.presenter
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.inputmethod.InputMethodManager
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
@@ -29,11 +33,29 @@ class MainActivity : AppCompatActivity(), OnItemClickListener {
         initRecyclerView()
         vm.getGifs(q = "hello")
 
+        binding.textInput.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {
+                if (s.isNotEmpty() && s[s.length - 1] == '\n' && s[0] != ' ' && s[0] != '\n') {
+                    val searchText = binding.textInput.text.toString()
+                    binding.textInput.setText(searchText.substring(0, searchText.length - 1))
+                    vm.getGifs(q = searchText)
+                } else if(s.isNotEmpty() && (s[0] == ' ' || s[0] == '\n')) {
+                    binding.textInput.text?.clear()
+                }
+            }
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+        })
+
         binding.buttonSearch.setOnClickListener {
-            vm.getGifs(q = binding.textInput.text.toString())
+            if(binding.textInput.text.toString().isNotEmpty()) {
+                val searchText = binding.textInput.text.toString()
+                vm.getGifs(q = searchText)
+            }
         }
 
         vm.liveDataGifs.observe(this) {
+            clearFocusAndHideKeyboard()
             adapter.submitList((vm.liveDataGifs.value as Gifs).listOfUrls)
         }
     }
@@ -72,5 +94,11 @@ class MainActivity : AppCompatActivity(), OnItemClickListener {
         val intent = Intent(this, FullscreenGifActivity::class.java)
         intent.putExtra("url", url)
         startActivity(intent)
+    }
+
+    private fun clearFocusAndHideKeyboard() {
+        binding.textInput.clearFocus()
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(binding.textInput.windowToken, 0)
     }
 }
